65da90526cf22ed97639182cabc89d99875a058c
K'uh Pik <improvetheworld@yandex.ru>
2020-11-24 22:03:10 +0300

    Added collision tracking components

---
+++ Assets/Source/Scripts/Components/OnCollisionEnterComponent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+public class OnCollisionEnterComponent : MonoBehaviour
+{
+    public event Action<Transform> OnEnter;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        OnEnter?.Invoke(collision.transform);
+    }
+}
---
+++ Assets/Source/Scripts/Components/OnTriggerEnterComponent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+public class OnTriggerEnterComponent : MonoBehaviour
+{
+    public event Action<Transform> OnEnter;
+
+    void OnTriggerEnter(Collider other)
+    {
+        OnEnter?.Invoke(other.transform);
+    }
+}
