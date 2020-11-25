94cf8d25c2a2cbf07466198454fb8be1f37c01a8
K'uh Pik <improvetheworld@yandex.ru>
2020-11-25 20:23:56 +0300

    Cell-data preparations and level generation for testing

---
+++ Assets/Source/Scripts/Components/CellComponent.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+//Объект должен висеть на пустышке и управлять объектами внутри себя
+public class CellComponent : MonoBehaviour
+{
+    [field: SerializeField] public GameObject Cell { get; private set; } //Основной объект
+    [field: SerializeField] public GameObject Trigger { get; private set; } //Триггер. Включается, если реальный объект внизу (нужен для краски)
+    [field: SerializeField] public MeshRenderer Renderer { get; private set; }
+    [field: SerializeField] public Color Color { get; private set; }
+    [field: SerializeField] public bool IsDown { get; private set; }
+
+    //Только обновляем информацию. Красить будем из системы.
+    public void SetColor(Color color)
+    {
+        Color = color;
+    }
+
+    //Тоже самое, только информация.
+    public void SetState(bool isDown)
+    {
+        IsDown = isDown;
+    }
+}
---
+++ Assets/Source/Scripts/Components/LevelCreatorComponent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+[Obsolete] //Используется только для первых тестов механики. Далее лвлы должны собираться руками.
+public class LevelCreatorComponent : MonoBehaviour
+{
+    [SerializeField] GameObject cellPrefab;
+    [SerializeField] Vector2Int borders;
+    [SerializeField] Vector3Int position;
+    [SerializeField] int scale;
+
+    void Awake()
+    {
+        for (int i = 0; i < borders.x; i++)
+        {
+            for (int j = 0; j < borders.y; j++)
+            {
+                Instantiate(cellPrefab, new Vector3(i, 0, j), Quaternion.identity, transform);
+            }
+        }
+
+        transform.localScale = new Vector3(scale, 1, scale);
+        transform.position = position;
+    }
+}
