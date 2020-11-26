5f40eb127eef045e89368de2aa1fac09770cd156
K'uh Pik <improvetheworld@yandex.ru>
2020-11-26 22:41:05 +0300

    Hex field

--- Assets/Source/Scripts/Components/LevelCreatorComponent.cs
+++ Assets/Source/Scripts/Components/LevelCreatorComponent.cs
@@ -4,18 +4,29 @@ using UnityEngine;
 [Obsolete] //Используется только для первых тестов механики. Далее лвлы должны собираться руками.
 public class LevelCreatorComponent : MonoBehaviour
 {
+    [Header("Level")]
     [SerializeField] GameObject cellPrefab;
     [SerializeField] Vector2Int borders;
     [SerializeField] Vector3Int position;
     [SerializeField] int scale;
 
+    [Header("Cell")]
+    [SerializeField] float xOffset;
+    [SerializeField] float zOffset;
+
     void Awake()
     {
-        for (int i = 0; i < borders.x; i++)
+        for (int i = 0; i < borders.y; i++)
         {
-            for (int j = 0; j < borders.y; j++)
+            var additionalXoffset = (i % 2) * (xOffset / 2f);
+            Debug.Log(additionalXoffset);
+
+            for (int j = 0; j < borders.x; j++)
             {
-                Instantiate(cellPrefab, new Vector3(i, 0, j), Quaternion.identity, transform);
+                float x = xOffset * j + additionalXoffset;
+                float z = zOffset * i;
+
+                Instantiate(cellPrefab, new Vector3(x, 0, z), Quaternion.identity, transform);
             }
         }
 
