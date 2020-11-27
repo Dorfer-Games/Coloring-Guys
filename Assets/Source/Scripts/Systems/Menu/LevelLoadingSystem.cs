c3d6d6c58158577269246de7d50dcf7e88772efd
K'uh Pik <improvetheworld@yandex.ru>
2020-11-27 19:55:09 +0300

    Fixes for DoTween without safe mod

--- Assets/Source/Scripts/Systems/Menu/LevelLoadingSystem.cs
+++ Assets/Source/Scripts/Systems/Menu/LevelLoadingSystem.cs
@@ -1,10 +1,18 @@
-﻿using Kuhpik;
+﻿using DG.Tweening;
+using Kuhpik;
+using Supyrb;
 using System.Linq;
 
-public class LevelLoadingSystem : GameSystem, IIniting
+public class LevelLoadingSystem : GameSystem, IIniting, IDisposing
 {
     void IIniting.OnInit()
     {
+        Signals.Clear();
         game.cellDictionary = FindObjectsOfType<CellComponent>().ToDictionary(x => x.transform, x => x);
+    }
+
+    void IDisposing.OnDispose()
+    {
+        DOTween.KillAll();
     }
 }
