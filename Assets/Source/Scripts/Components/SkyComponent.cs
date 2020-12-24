9ef814776d31ac9dce34b5afeb9b332c683a1b88
Артур <Kedrovskiy.artur@gmail.com>
2020-12-24 13:19:25 +0300

    no message

---
+++ Assets/Source/Scripts/Components/SkyComponent.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+using System;
+
+
+
+[Serializable]
+public class SkySettings
+{
+    public Color topColor;
+    public Color horizonColor;
+    public Color bottomColor;
+}
+
+public class SkyComponent : MonoBehaviour
+{
+    [SerializeField] private Material sky;
+
+    [Header("Цветовая настройка неба")]
+    [SerializeField] private SkySettings[] skySettings;
+
+    private void Start()
+    {
+        SetSettingsSky();
+    }
+    private void SetSettingsSky()
+    {
+        RenderSettings.skybox.SetColor("_Tint", skySettings[0].topColor);
+        var block = new MaterialPropertyBlock();
+        block.SetColor("Top Color", skySettings[0].topColor);
+        block.SetVector("Horizon Color", skySettings[0].horizonColor);
+        block.SetVector("Bottom Color", skySettings[0].bottomColor);
+        
+    }
+}
