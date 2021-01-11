59a15b5e78a1df689404c641f1af155faec2f58c
Артур <Kedrovskiy.artur@gmail.com>
2021-01-11 13:10:26 +0300

    Изменил модель игрока(анимации пока не работают) Изменил визуал стаков Изменил время падения гексов

---
+++ Assets/Source/Scripts/Components/OnTriggerCellChange.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTriggerCellChange : MonoBehaviour
+{
+    [SerializeField] private CellComponent cellComponent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.TryGetComponent<ColorStackComponent>(out var component))
+        {
+            if (cellComponent.IsDown) {
+                component.UpdatePosition();
+            }
+        }
+    }
+}
