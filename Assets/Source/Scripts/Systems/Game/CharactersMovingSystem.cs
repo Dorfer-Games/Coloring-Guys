d8c1be633e4932fcf9d46261835db1baa6761792
iamvkosarev <iamvkosrev@gmail.com>
2021-01-25 18:17:08 +0300

    Merge branch 'Develop' into Stickman-Fall-Dev

--- Assets/Source/Scripts/Systems/Game/CharactersMovingSystem.cs
+++ Assets/Source/Scripts/Systems/Game/CharactersMovingSystem.cs
@@ -19,18 +19,14 @@ public class CharactersMovingSystem : GameSystem, IFixedUpdating, IIniting
         if (StartGame) {
             for (int i = 0; i < game.characters.Length; i++)
             {
+
+
+                var rotation = Vector3.up * config.GetValue(EGameValue.RotationSpeed) * game.characters[i].rotationValue * Time.fixedDeltaTime;
                 var movement = Vector3.forward * config.GetValue(EGameValue.MoveSpeed) * Time.fixedDeltaTime;
 
                 if (speedupMainCharacter && game.characters[i].isPlayer) movement *= config.GetValue(EGameValue.PlayerSpeedX);
-                if (i == 0)
-                {
-                    game.characters[0].rigidbody.transform.eulerAngles = game.playerRotation;
-                }
-                else
-                {
-                    var rotation = Vector3.up * config.GetValue(EGameValue.RotationSpeed) * game.characters[i].rotationValue * Time.fixedDeltaTime;
-                    game.characters[i].rigidbody.transform.Rotate(rotation);
-                }
+                
+                game.characters[i].rigidbody.transform.Rotate(rotation);
                 game.characters[i].rigidbody.transform.Translate(movement, Space.Self);
                 game.characters[i].animator.SetBool("idle", false);
                 game.characters[i].animator.SetFloat("Dir", game.characters[i].rotationValue);
