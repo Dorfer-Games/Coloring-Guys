a29c977ee66a4a4f3c09745a62e745aeb1519fa7
Kuhpik <improvetheworld@yandex.ru>
2021-04-21 11:35:25 +0300

    Fix for money animation delay

--- Assets/Source/Scripts/Systems/Finish/MoneyRewardedSystem.cs
+++ Assets/Source/Scripts/Systems/Finish/MoneyRewardedSystem.cs
@@ -1,17 +1,17 @@
-﻿using UnityEngine;
-
-using System.Collections;
-using Kuhpik;
-using TMPro;
+﻿using Kuhpik;
+using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class MoneyRewardedSystem : GameSystem
 {
     public static MoneyRewardedSystem rewardedSystem { get; private set; }
     public Transform startPoint_X5, startPoint_No; // Точки, с которых будут стартовать анимация монеток
+
     [SerializeField] private GameObject moneyAnimation;
     [SerializeField] private Button Next, NoThinks;
-    private void Start()
+
+    void Start()
     {
         if (rewardedSystem == null) rewardedSystem = this;
     }
@@ -23,19 +23,19 @@ public class MoneyRewardedSystem : GameSystem
         StartCoroutine(StartAnimationRewarded(moneyCount));
     }
 
-
-    private void AddMoney(int moneyCount)
+    void AddMoney(int moneyCount)
     {
         player.money += moneyCount;
         Bootstrap.GetSystem<MoneyUIComponent>().UpdateMoney.Invoke(player.money);
     }
-    private IEnumerator StartAnimationRewarded(int moneyCount)
+
+    IEnumerator StartAnimationRewarded(int moneyCount)
     {
         Next.enabled = false;
         NoThinks.enabled = false;
-        yield return new WaitForSeconds(1.7f);
-        AddMoney(moneyCount);
         yield return new WaitForSeconds(0.5f);
+        AddMoney(moneyCount);
+        yield return new WaitForSeconds(2f);
         Bootstrap.GameRestart(0);
     }
 }
