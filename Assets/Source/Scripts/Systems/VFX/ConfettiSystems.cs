104c8b481463cbcac012c88d171afa5d163c4344
Kuhpik <improvetheworld@yandex.ru>
2021-04-30 07:40:09 +0300

    Dance animation should be fixed

--- Assets/Source/Scripts/Systems/VFX/ConfettiSystems.cs
+++ Assets/Source/Scripts/Systems/VFX/ConfettiSystems.cs
@@ -5,31 +5,34 @@ using UnityEngine;
 public class ConfettiSystems : GameSystem, IIniting
 {
     [SerializeField] private ParticleSystem[] confetti;
-
     [SerializeField] private float timeNextScreenStart = 1.5f;
+
     public void OnInit()
     {
         if (game.isVictory)
         {
             StartCoroutine(offsetTimeStartConfetti());
-            game.characters[0].animator.SetBool("Dance", true);
+            //game.characters[0].animator.SetBool("Dance", true);
+            game.characters[0].animator.Play("Samba Dancing");
         }
-        else
-            Bootstrap.ChangeGameState(EGamestate.RateUs);
+
+        else Bootstrap.ChangeGameState(EGamestate.RateUs);
     }
 
     private IEnumerator offsetTimeStartConfetti()
     {
         yield return new WaitForSeconds(0.5f);
-            for (int b = 0; b < confetti.Length; b++)
-            {
-                confetti[b].Play();
-            }
-            StartCoroutine(FinisshUIStart());
+
+        for (int b = 0; b < confetti.Length; b++)
+        {
+            confetti[b].Play();
+        }
+
+        StartCoroutine(FinisshUIStart());
     }
     private IEnumerator FinisshUIStart()
     {
-       yield return new WaitForSeconds(timeNextScreenStart);
+        yield return new WaitForSeconds(timeNextScreenStart);
         Bootstrap.ChangeGameState(EGamestate.RateUs);
     }
 }
